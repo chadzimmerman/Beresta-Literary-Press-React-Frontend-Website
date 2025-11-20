790b5534b3aa393a3fed4f4fe77a234bb46b18a7
137725897+chadzimmerman@users.noreply.github.com <bookingpedestals@gmail.com>
2025-11-20 13:49:12 +0300

    Added CORS handling in api/books/index.js and api/books/search.js

--- api/books/index.js
+++ api/books/index.js
@@ -2,6 +2,12 @@
 const { getPool } = require("../_db");
 
 module.exports = async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
   if (req.method !== "GET") {
     res.setHeader("Allow", "GET");
     return res.status(405).end("Method Not Allowed");
--- api/books/search.js
+++ api/books/search.js
@@ -2,6 +2,16 @@
 const { getPool } = require("../_db");
 
 module.exports = async (req, res) => {
+
+    // CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  
   if (req.method !== "GET") {
     res.setHeader("Allow", "GET");
     return res.status(405).end("Method Not Allowed");
